5e12528f8a8dd5a8ea38afac6d4ef911e4dc1c56
Vladyslav Bagatskyy <vladyslavbagatskyy@gmail.com>
2024-12-02 19:52:09 +0000

    test1

--- hello.py
+++ hello.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/hello")
+def hello_world1():
+    return "<p>I am a sigma sigma John Pork!</p>"
