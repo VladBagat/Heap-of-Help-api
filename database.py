fb3657fe49514d99297a71220fdf6baf72b874b1
Vladyslav Bagatskyy <vladyslavbagatskyy@gmail.com>
2025-03-03 14:16:34 +0000

    Updated tag and tutor data fetching logic to match exisitng registration/profile infrastructure

--- database.py
+++ database.py
@@ -42,18 +42,9 @@ def tags_table_setup(con : connection):
     with con.cursor() as cur:
         
         user_tags = sql.SQL(
-            """CREATE TABLE IF NOT EXISTS user_tags 
-            (user_tag_id serial PRIMARY KEY,
+            """CREATE TABLE IF NOT EXISTS tags 
+            (tag_id serial PRIMARY KEY,
             user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
-            tag1 INT,
-            tag2 INT,
-            tag3 INT,
-            tag4 INT,
-            tag5 INT);""")
-        item_tags = sql.SQL(
-            """CREATE TABLE IF NOT EXISTS item_tags 
-            (item_tag_id serial PRIMARY KEY,
-            item_id INT NOT NULL REFERENCES items(id) ON DELETE CASCADE ON UPDATE CASCADE,
             tag1 INT,
             tag2 INT,
             tag3 INT,
@@ -61,18 +52,35 @@ def tags_table_setup(con : connection):
             tag5 INT);""")
         
         cur.execute(user_tags)
-        cur.execute(item_tags)
         con.commit()
-        
+
 @db_conn.with_conn
-def items_table_setup(con : connection):
-    with con.cursor() as cur:
-        cur.execute("""CREATE TABLE IF NOT EXISTS items (
-            id serial PRIMARY KEY,
-            name varchar (150) NOT NULL);""")
-        con.commit()
+def fetch_user_tags(conn : connection, user_id):
+    with conn.cursor() as cur:
+        cur.execute(sql.SQL(
+            """SELECT tag1, tag2, tag3, tag4, tag5
+            FROM tags JOIN users ON tags.user_id = users.id
+            WHERE users.username = {user_id};"""
+        ).format(user_id=sql.Literal(user_id)))
+        return cur.fetchall()
 
+@db_conn.with_conn
+def fetch_tutor_tags(conn : connection):
+    with conn.cursor() as cur:
+        cur.execute('''SELECT t.* FROM tags t
+                    JOIN tutors tut ON t.user_id = tut.id
+                    ORDER BY RANDOM() LIMIT 100;''')
 
+        return cur.fetchall()
+    
+@db_conn.with_conn 
+def fetch_recommended_tutors(conn : connection, item_id_list : list):
+    with conn.cursor() as cur:
+        query = """SELECT first_name, last_name, description, profile_img 
+        FROM tutors WHERE id IN %s;"""
+        execute_values(cur, query, (tuple(item_id_list),))
+        return cur.fetchall()
+        
 @db_conn.with_conn 
 def register_user_db(con : connection, request_username, hashed_password):
     with con.cursor() as cur:
@@ -108,12 +116,6 @@ def login_user_db(con: connection, request_username, request_password):
                           hashed_pass[0].encode('utf-8')):
             return 200
         return 401
-        
-@db_conn.with_conn
-def fetch_item_tags(conn : connection):
-    with conn.cursor() as cur:
-        cur.execute(sql.SQL("SELECT * FROM item_tags ORDER BY RANDOM() LIMIT 100;"))
-        return cur.fetchall()
 
 @db_conn.with_conn
 def tutees_table_setup(con : connection):
@@ -177,7 +179,6 @@ def get_tutor_profile(con : connection):
             return None  # No data found
 
 @db_conn.with_conn
-
 def is_tutor(con : connection, request_username):
     with con.cursor() as cur:
         cur.execute(sql.SQL(
@@ -196,28 +197,6 @@ def is_tutor(con : connection, request_username):
             return False
         else:
             return True
-
-def fetch_user_tags(conn : connection, user_id):
-    with conn.cursor() as cur:
-        cur.execute(sql.SQL(
-            """SELECT user_tags.tag1,
-                user_tags.tag2,
-                user_tags.tag3,
-                user_tags.tag4,
-                user_tags.tag5
-            FROM user_tags JOIN users ON user_tags.user_id = users.id
-            WHERE users.username = {user_id};"""
-        ).format(user_id=sql.Literal(user_id)))
-        return cur.fetchall()
-    
-@db_conn.with_conn 
-def fetch_recommended_items(conn : connection, item_id_list : list):
-    with conn.cursor() as cur:
-        query = """SELECT items.name 
-        FROM item_tags JOIN items ON item_tags.item_id = items.id
-        WHERE items.id IN %s;"""
-        execute_values(cur, query, (tuple(item_id_list),))
-        return cur.fetchall()
 
 @db_conn.with_conn
 def fetch_test(conn : connection):
